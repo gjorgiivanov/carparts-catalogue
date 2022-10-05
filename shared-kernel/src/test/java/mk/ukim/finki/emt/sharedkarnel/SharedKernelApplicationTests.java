54b341cf9f62c9bca741cd5de8d90ddee7845ecb
gorgiivanov <gorgi.ivanov01@gmail.com>
2022-10-05 22:15:26 +0200

    Connect modules

--- shared-kernel/src/test/java/mk/ukim/finki/emt/sharedkarnel/SharedKernelApplicationTests.java
+++ shared-kernel/src/test/java/mk/ukim/finki/emt/sharedkarnel/SharedKernelApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SharedKarnelApplicationTests {
+class SharedKernelApplicationTests {
 
     @Test
     void contextLoads() {
