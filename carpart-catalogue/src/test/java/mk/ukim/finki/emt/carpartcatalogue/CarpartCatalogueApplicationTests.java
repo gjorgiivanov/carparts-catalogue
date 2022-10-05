e432af6baa39c81f1aa9da606215395e798f6791
gorgiivanov <gorgi.ivanov01@gmail.com>
2022-10-05 16:05:16 +0200

    Add all modules

---
+++ carpart-catalogue/src/test/java/mk/ukim/finki/emt/carpartcatalogue/CarpartCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.carpartcatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpartCatalogueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
